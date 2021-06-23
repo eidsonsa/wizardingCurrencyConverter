d21798c26528968c0a4754b56f09c61947cfe415
Eidson <ejas@cin.ufpe.br>
2021-06-23 12:06:39 -0300

    converter done

--- App.tsx
+++ App.tsx
@@ -17,6 +17,8 @@
    Text,
    useColorScheme,
    View,
+   KeyboardAvoidingView,
+   TextInput
  } from 'react-native';
 
  import {
@@ -27,88 +29,42 @@
    ReloadInstructions,
  } from 'react-native/Libraries/NewAppScreen';
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
 
  const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
+  const [galleons, setGalleons] = React.useState(0);
+  const [sickles, setSickles] = React.useState(galleons / 17);
+  const [knuts, setKnuts] = React.useState(sickles / 29);
+
+  const setValue = function(val: string){
+    setGalleons(+val);
+    setSickles(+val / 17);
+    setKnuts(+val / 29);
+  }
 
    return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
+     <SafeAreaView>
+      <Text> Wizarding Currency Converter</Text>
+      <KeyboardAvoidingView>
+        <TextInput 
+        style = {styles.input} 
+        value = {galleons.toString()}
+        onChangeText = {setValue} 
+        placeholder = "Galleons"
+        />
+      </KeyboardAvoidingView>
+      <View>
+        <Text> {sickles} </Text>
+        <Text> {knuts} </Text>
+      </View>
      </SafeAreaView>
-   );
+   )
+
  };
 
  const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
+   input: {
+
    },
  });
 
