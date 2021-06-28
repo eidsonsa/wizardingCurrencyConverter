ee71e3e263d58972dc5af0de1bae09f2e065f6db
Eidson <ejas@cin.ufpe.br>
2021-06-28 11:31:14 -0300

    sections added to list

--- App.tsx
+++ App.tsx
@@ -18,7 +18,9 @@
    TextInput,
    FlatList,
    Modal,
-   TouchableOpacity
+   TouchableOpacity,
+   SectionList,
+   ScrollView
  } from 'react-native';
 
  import AsyncStorage from '@react-native-community/async-storage'
@@ -52,44 +54,54 @@
 
   const DATA = [
     {
-      id: 'GAL',
-      val: galleons.toFixed(2),
-      title: 'galleon',
-      ...(galleons != 1 && {title: 'galleons'}),
-      conversion: '1 GAL = 1 GAL'
+      title: 'Wizard',
+      data: [
+        {
+          id: 'GAL',
+          val: galleons.toFixed(2),
+          title: 'galleon',
+          ...(galleons != 1 && {title: 'galleons'}),
+          conversion: '1 GAL = 1 GAL'
+        },
+        {
+          id: 'SIC',
+          val: (galleons / 17).toFixed(2),
+          title: 'sickle',
+          ...(+(galleons / 17).toFixed(2) != 1 && {title: 'sickles'}),
+          conversion: '1 SIC = 17 GAL'
+        },
+        {
+          id: 'KNU',
+          val: (galleons / 493).toFixed(2),
+          title: 'knut',
+          ...(+(galleons / 493).toFixed(2) != 1 && {title: 'knuts'}),
+          conversion: '1 KNU = 493 GAL'
+        },
+      ]
     },
     {
-      id: 'SIC',
-      val: (galleons / 17).toFixed(2),
-      title: 'sickle',
-      ...(+(galleons / 17).toFixed(2) != 1 && {title: 'sickles'}),
-      conversion: '1 SIC = 17 GAL'
-    },
-    {
-      id: 'KNU',
-      val: (galleons / 493).toFixed(2),
-      title: 'knut',
-      ...(+(galleons / 493).toFixed(2) != 1 && {title: 'knuts'}),
-      conversion: '1 KNU = 493 GAL'
-    },
-    {
-      id: 'USD',
-      val: (galleons * 6.64).toFixed(2),
-      title: 'USD',
-      conversion: '1 GAL = 6.64 USD'
-    },
-    {
-      id: 'EUR',
-      val: (galleons * 5.58).toFixed(2),
-      title: 'EUR',
-      conversion: '1 GAL = 5.58 EUR'
-    },
-    {
-      id: 'BRL',
-      val: (galleons * 21.64).toFixed(2),
-      title: 'BRL',
-      conversion: '1 GAL = 21.64 BRL'
-    }
+      title: 'Muggle',
+      data: [
+        {
+          id: 'USD',
+          val: (galleons * 6.64).toFixed(2),
+          title: 'USD',
+          conversion: '1 GAL = 6.64 USD'
+        },
+        {
+          id: 'EUR',
+          val: (galleons * 5.58).toFixed(2),
+          title: 'EUR',
+          conversion: '1 GAL = 5.58 EUR'
+        },
+        {
+          id: 'BRL',
+          val: (galleons * 21.64).toFixed(2),
+          title: 'BRL',
+          conversion: '1 GAL = 21.64 BRL'
+        }
+      ]
+    }   
   ]
 
   const saveData = async () => {
@@ -126,7 +138,7 @@
   
 
    return (
-     <SafeAreaView style={styles.container}>
+     <ScrollView style={styles.container}>
 
       <Modal
       visible = {modalVisible}
@@ -152,15 +164,16 @@
         onChangeText = {setValue} 
         />
       </KeyboardAvoidingView>
-      <View style={styles.listView}>
+      <View>
         <Text style={styles.listHeader}> Converted Currency: </Text>
-        <FlatList 
-        data = {DATA}
+        <SectionList 
+        sections = {DATA}
         keyExtractor = {item => item.id}
         renderItem={({ item }) => Item(item)}
+        renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
         />
       </View>
-     </SafeAreaView>
+     </ScrollView>
    )
 
  };
@@ -172,12 +185,13 @@
      width: '100%',
    },
    title: {
-    fontSize: 20,
+    fontSize: 22,
     textAlign: 'center',
-    margin: 10
+    margin: 10,
+    marginBottom: 20
    },
    galleons: {
-     fontSize: 16,
+     fontSize: 18,
      textAlign: 'center',
      marginBottom: 10
    },
@@ -190,11 +204,19 @@
      backgroundColor: '#a48b5a',
      color: '#000'
    },
-   listView: {
-    
+   sectionHeader: {
+    backgroundColor: '#55482f',
+    color: '#cfc2a9',
+    marginVertical: 5,
+    marginHorizontal: 10,
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    fontSize: 18,
+    textAlign: 'center',
    },
    listHeader: {
-    fontSize: 16,
+    fontSize: 18,
     textAlign: 'center',
     marginVertical: 10
    },
